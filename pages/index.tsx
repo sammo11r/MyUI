231050f5b88df2556574cd601e8b6a87abfc0541
Thoumoung, Y <y.thoumoung@student.tue.nl>
2022-05-17 15:43:45 +0200

    58a wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,31 +2,83 @@
 import React from 'react';
 import logo from '../public/logo.svg';
 import Image from 'next/image';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import 'antd/dist/antd.css';
+// import './styles/index.css';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+
+// import { Content, Footer, Header } from 'antd/lib/layout/layout';
+// import Sider from 'antd/lib/layout/Sider';
+import { icons } from 'antd/lib/image/PreviewGroup';
+
+const { Header, Content, Sider } = Layout;
+const items1 = ['1', '2', '3'].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+  const key = String(index + 1);
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(icon),
+    label: `subnav ${key}`,
+    children: new Array(4).fill(null).map((_, j) => {
+      const subKey = index * 4 + j + 1;
+      return {
+        key: subKey,
+        label: `option${subKey}`,
+      };
+    }),
+  };
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Image
-          src={logo}
-          alt="logo"
-          className="App-logo"
-          width={500}
-          height={500}
+    <Layout>
+    <Header className="header">
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+    </Header>
+    <Layout>
+      <Sider width={200} className="site-layout-background">
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{
+            height: '100%',
+            borderRight: 0,
+          }}
+          items={items2}
         />
-        <p>
-          Edit <code>pages/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      </Sider>
+      <Layout
+        style={{
+          padding: '0 24px 24px',
+        }}
+      >
+        <Breadcrumb
+          style={{
+            margin: '16px 0',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  </Layout>
   );
 }
 
