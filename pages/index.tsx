fb233266b1a7e017763e2b5cbff04dc004e492b2
Thoumoung, Y <y.thoumoung@student.tue.nl>
2022-05-19 01:57:35 +0300

    Modified top and side bars

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,37 +5,64 @@ import Image from 'next/image';
 import { Breadcrumb, Layout, Menu } from 'antd';
 import 'antd/dist/antd.css';
 // import './styles/index.css';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { UserOutlined, TableOutlined, NotificationOutlined, SettingFilled, PicCenterOutlined } from '@ant-design/icons';
 import {
   signIn,
   signOut,
   useSession
 } from 'next-auth/react';
 
-// import { Content, Footer, Header } from 'antd/lib/layout/layout';
+// import { Header, Content } from 'antd/lib/layout/layout';
 // import Sider from 'antd/lib/layout/Sider';
 import { icons } from 'antd/lib/image/PreviewGroup';
 
 const { Header, Content, Sider } = Layout;
-const items1 = ['1', '2', '3'].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
-  const key = String(index + 1);
+
+// Commented lines are from online template
+
+// const items1 = ['1', '2', '3'].map((key) => ({
+//   key,
+//   label: `nav ${key}`,
+// }));
+// const items2 = [TableOutlined, PicCenterOutlined].map((icon, index) => {
+//   const key = String(index + 1);
+//   return {
+//     key: `sub${key}`,
+//     icon: React.createElement(icon),
+//     label: `Dashboard ${key}`,
+//     children: new Array(4).fill(null).map((_, j) => {
+//       const subKey = index * 4 + j + 1;
+//       return {
+//         key: subKey,
+//         label: `option${subKey}`,
+//       };
+//     }),
+//   };
+// });
+
+function getItem(label: any, key: any, icon: any, children: any) {
   return {
-    key: `sub${key}`,
-    icon: React.createElement(icon),
-    label: `subnav ${key}`,
-    children: new Array(4).fill(null).map((_, j) => {
-      const subKey = index * 4 + j + 1;
-      return {
-        key: subKey,
-        label: `option${subKey}`,
-      };
-    }),
-  };
-});
+    key,
+    icon,
+    children,
+    label,
+    };
+}
+
+const itemsDashboard= [
+  getItem('Base Tables', 'baseTables', <TableOutlined />, [
+    getItem('Table 1', 'table1', null, null),
+    getItem('Table 2', 'table2', null, null),
+    getItem('Table 3', 'table3', null, null),
+    getItem('Table 4', 'table4', null, null), 
+  ]),
+  getItem('Dashboards', 'dashboards', <PicCenterOutlined />, [
+    getItem('Dashboard 1', 'dashboard1', null, null),
+    getItem('Dashboard 2', 'dashboard2', null, null),
+    getItem('Dashboard 3', 'dashboard3', null, null),
+    getItem('Dashboard 4', 'dashboard4', null, null), 
+  ]),
+];
 
 function App() {
 
@@ -47,8 +74,28 @@ function App() {
         <Content className="header-logo">
           MyUI
         </Content>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1}/>
+        {/* <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1}/> */}
+        <SettingFilled id='dashboard-settings'
+          style={{ 
+            position: 'relative',
+            top: 16,
+            float: 'right',
+            right: 20,
+            fontSize: "30px", 
+            color: "white" 
+            }}/>
+            <UserOutlined id='dashboard-user-profile'
+          style={{ 
+            position: 'relative',
+            top: 16,
+            float: 'right',
+            left: 50,
+            fontSize: "30px",
+            color: "white" 
+            }}/>
+          
       </Header>
+
       <Layout>
         <Sider width={200} className="site-layout-background">
           <Menu
@@ -59,7 +106,7 @@ function App() {
               height: '100%',
               borderRight: 0,
             }}
-            items={items2}
+            items={itemsDashboard}
           />
         </Sider>
         <Layout
@@ -67,7 +114,7 @@ function App() {
             padding: '0 24px 24px',
           }}
         >
-          <Breadcrumb
+          {/* <Breadcrumb
             style={{
               margin: '16px 0',
             }}
@@ -75,7 +122,7 @@ function App() {
             <Breadcrumb.Item>Home</Breadcrumb.Item>
             <Breadcrumb.Item>List</Breadcrumb.Item>
             <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
+          </Breadcrumb> */}
           <Content
             className="site-layout-background"
             style={{
@@ -84,10 +131,11 @@ function App() {
               minHeight: 280,
             }}
           >
-            Content
+            Dashboard
           </Content>
         </Layout>
       </Layout>
+
     </Layout>
   );
 }
