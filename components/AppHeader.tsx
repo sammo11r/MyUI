b1d4b3e91f7268ae317385633cc29ea8d2eb4ba1
Farla, R <r.farla@student.tue.nl>
2022-05-19 12:00:01 +0200

    Actually add the component files

---
+++ components/AppHeader.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import 'antd/dist/antd.css';
+import { UserOutlined, SettingFilled } from '@ant-design/icons';
+import { Content, Header } from 'antd/lib/layout/layout';
+
+export function AppHeader() {
+  return (
+    <Header className='header'>
+      <Content className='header-logo'>
+        MyUI
+      </Content>
+      <SettingFilled id='header-settings'
+        style={{
+          position: 'relative',
+          top: 16,
+          float: 'right',
+          right: 20,
+          fontSize: "30px",
+          color: "white"
+        }} />
+      <UserOutlined id='header-user-profile'
+        style={{
+          position: 'relative',
+          top: 16,
+          float: 'right',
+          left: 50,
+          fontSize: "30px",
+          color: "white"
+        }} />
+    </Header>
+  );
+}
