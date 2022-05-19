f34f4cdd1a7ac2af5be0aff9b48b50819304ec67
Jeroen Hellenbrand <j.w.f.hellenbrand@student.tue.nl>
2022-05-19 12:09:37 +0200

    feat(pages tables and dashboard)

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,21 @@ import { Layout, Menu } from 'antd';
 import 'antd/dist/antd.css';
 import { TableOutlined, PicCenterOutlined } from '@ant-design/icons';
 import {
-  signIn,
-  signOut,
   useSession
 } from 'next-auth/react';
+import Link from 'next/link';
 
-import { AppHeader } from '../components/AppHeader'
+import { AppHeader } from '../components/AppHeader';
 
 const { Content, Sider } = Layout;
 
+/**
+ * @param {*} label
+ * @param {*} key
+ * @param {*} icon
+ * @param {*} children
+ * @return {*}
+ */
 function getItem(label: any, key: any, icon: any, children: any) {
   return {
     key,
@@ -24,21 +30,43 @@ function getItem(label: any, key: any, icon: any, children: any) {
 
 const itemsDashboard = [
   getItem('Base Tables', 'baseTables', <TableOutlined />, [
-    getItem('Table 1', 'table1', null, null),
-    getItem('Table 2', 'table2', null, null),
-    getItem('Table 3', 'table3', null, null),
-    getItem('Table 4', 'table4', null, null),
+    getItem(<Link href='/table/1'>Table 1</Link>, 'table1', null, null),
+    getItem(<Link href='/table/2'>Table 2</Link>, 'table2', null, null),
+    getItem(<Link href='/table/3'>Table 3</Link>, 'table3', null, null),
+    getItem(<Link href='/table/4'>Table 4</Link>, 'table4', null, null),
   ]),
   getItem('Dashboards', 'dashboards', <PicCenterOutlined />, [
-    getItem('Dashboard 1', 'dashboard1', null, null),
-    getItem('Dashboard 2', 'dashboard2', null, null),
-    getItem('Dashboard 3', 'dashboard3', null, null),
-    getItem('Dashboard 4', 'dashboard4', null, null),
+    getItem(
+        <Link href='/dashboard/1'>Dashboard 1</Link>,
+        'dashboard1',
+        null,
+        null
+    ),
+    getItem(
+        <Link href='/dashboard/2'>Dashboard 2</Link>,
+        'dashboard2',
+        null,
+        null
+    ),
+    getItem(
+        <Link href='/dashboard/3'>Dashboard 3</Link>,
+        'dashboard3',
+        null,
+        null
+    ),
+    getItem(
+        <Link href='/dashboard/4'>Dashboard 4</Link>,
+        'dashboard4',
+        null,
+        null
+    ),
   ]),
 ];
 
+/**
+ * @return {*} 
+ */
 function App() {
-
   const { data: session, status } = useSession()
 
   return (
@@ -72,7 +100,6 @@ function App() {
               minHeight: 280,
             }}
           >
-            Dashboard
           </Content>
         </Layout>
       </Layout>
@@ -82,6 +109,6 @@ function App() {
 }
 
 // Make sure this page is protected
-App.auth = true
+App.auth = true;
 
 export default App;
