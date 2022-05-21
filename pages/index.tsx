627cc4a68f558ca24159e555085fff480033d7d8
Kustermans. Q <q.kustermans@student.tue.nl>
2022-05-21 08:37:20 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 import {
@@ -96,51 +95,10 @@ function App() {
         </Layout>
       </Layout>
     </Layout>
-=======
-/* eslint-disable require-jsdoc */
-import React from "react";
-import { useSession } from "next-auth/react";
-import Image from "next/image";
-
-import logo from "../public/logo.svg";
-
-function App() {
-  // TODO: Persist user data after succesful signin.
-  // eslint-disable-next-line no-unused-vars
-  const { data: session, status } = useSession();
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Image
-          src={logo}
-          alt="logo"
-          className="App-logo"
-          width={500}
-          height={500}
-        />
-        <p>
-          Edit <code>pages/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> master
   );
 }
 
 // Make sure this page is protected
-<<<<<<< HEAD
 App.auth = false;
-=======
-App.auth = true;
->>>>>>> master
 
 export default App;
