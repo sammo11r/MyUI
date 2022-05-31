60a88d713be019269c9bd006165835a086337e22
Daan Boelhouwers <daanboelhouwers@gmail.com>
2022-05-31 13:54:18 +0200

    Reimplement renderDashboardElement function

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -8,7 +8,7 @@ enum elementType {
   STATIC
 }
 
-function renderDashboardElement(element: any, hasuraProps: any): JSX.Element {
+function renderDashboardElement(element: any, hasuraProps: any, key: number): JSX.Element {
   // Style properties used to render element, 
   // position is absolute relative to parent element, i.e. WorkSpace
   const style = {
@@ -20,58 +20,103 @@ function renderDashboardElement(element: any, hasuraProps: any): JSX.Element {
     outline: "2px solid #ebf2ff",
   }
 
+  let rendered_element = <p>Unknown element type</p>;
   switch (element.type) {
     case elementType.GRIDVIEW:
-      return (
-        <p>Gridview W.I.P.</p>
-      )
+      rendered_element = <p>Gridview W.I.P.</p>;
+      break;
     case elementType.STATIC:
-      return (
-        <p>Gridview W.I.P.</p>
-      )
-    default:
-      return <p>Unknown element type</p>;
+      rendered_element = <p>Static W.I.P.</p>;
+      break;
   }
+
+  return (
+    <div
+      key={key}
+      style={{ outline: "2px solid #ebf2ff" }}
+      data-grid={{
+        x: element.position.x,
+        y: element.position.y,
+        w: element.width,
+        h: element.height
+      }}
+    >
+      {rendered_element}
+    </div>
+  );
 }
 
 function Dashboard({ hasuraProps, systemProps, name, mode, userConfig, setUserConfig }: any): JSX.Element {
-  const dashboards = []; //userConfig["dashboards"] ? userConfig["dashboards"] : [];
+  const dashboards = [{
+    name: "Cool Dashboard",
+    dashboardElements: [
+      {
+        name: "Cool Element",
+        position: {
+          x: 0,
+          y: 0
+        },
+        height: 9,
+        width: 6,
+        rowsPerPage: 5,
+        query: `
+        query MyQuery {
+          Product {
+            id
+            name
+            description
+          }
+        }
+        `,
+        type: elementType.GRIDVIEW // type of visualization
+      },
+      {
+        name: "Text Element",
+        position: {
+          x: 6,
+          y: 0
+        },
+        height: 9,
+        width: 6,
+        text: "Dit is een klein stukje tekst. Prijs de heer, zuip wat meer!",
+        type: elementType.STATIC
+      },
+      {
+        name: "Video Element",
+        position: {
+          x: 0,
+          y: 9
+        },
+        height: 9,
+        width: 6,
+        text: "https://archive.org/download/Rick_Astley_Never_Gonna_Give_You_Up/Rick_Astley_Never_Gonna_Give_You_Up.mp4",
+        type: elementType.STATIC
+      }]
+  }];
+  //userConfig["dashboards"] ? userConfig["dashboards"] : [];
 
   function getDashboard(name: string) {
     const dashboard = dashboards.filter((dashboard: any) => dashboard.name === name)[0];
     if (dashboard) {
       return dashboard
     }
-    return dashboards[0]
+    return dashboards[0];
   }
 
-  const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-    { i: "c", x: 4, y: 0, w: 1, h: 2 }
-  ];
-
   return (
-    /*<>
-      {
-        getDashboard(name)
-          .dashboardElements
-          .map((element: any) => renderDashboardElement(element, hasuraProps))
-      }
-    </>*/
     <GridLayout
       className="layout"
-      layout={layout}
       cols={12}
       rowHeight={30}
-      width={1200}
-      heigth={100}
+      width={1500}
       compactType={null}
       preventCollision={true}
     >
-      <div key="a">a</div>
-      <div key="b">b</div>
-      <div key="c">c</div>
+      {
+        getDashboard(name)
+          .dashboardElements
+          .map((element: any, index) => renderDashboardElement(element, hasuraProps, index))
+      }
     </GridLayout>
   );
 }
