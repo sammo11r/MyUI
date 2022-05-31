07fca32a07ad5ae1e3d411259c695ad289a8e33f
Daan Boelhouwers <daanboelhouwers@gmail.com>
2022-05-31 19:51:44 +0200

    Improve renderDashboardElement

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -8,56 +8,16 @@ enum elementType {
   STATIC
 }
 
-function renderDashboardElement(element: any, hasuraProps: any, key: number): JSX.Element {
-  // Style properties used to render element, 
-  // position is absolute relative to parent element, i.e. WorkSpace
-  const style = {
-    position: "absolute",
-    top: element.position.y,
-    left: element.position.x,
-    height: element.height,
-    width: element.width,
-    outline: "2px solid #ebf2ff",
-  }
-
-  let rendered_element = <p>Unknown element type</p>;
-  switch (element.type) {
-    case elementType.GRIDVIEW:
-      rendered_element = <p>Gridview W.I.P.</p>;
-      break;
-    case elementType.STATIC:
-      rendered_element = <p>Static W.I.P.</p>;
-      break;
-  }
-
-  return (
-    <div
-      key={key}
-      style={{ outline: "2px solid #ebf2ff" }}
-      data-grid={{
-        x: element.position.x,
-        y: element.position.y,
-        w: element.width,
-        h: element.height
-      }}
-    >
-      {rendered_element}
-    </div>
-  );
-}
-
 function Dashboard({ hasuraProps, systemProps, name, mode, userConfig, setUserConfig }: any): JSX.Element {
   const dashboards = [{
     name: "Cool Dashboard",
     dashboardElements: [
       {
         name: "Cool Element",
-        position: {
-          x: 0,
-          y: 0
-        },
-        height: 9,
-        width: 6,
+        x: 0,
+        y: 0,
+        h: 9,
+        w: 6,
         rowsPerPage: 5,
         query: `
         query MyQuery {
@@ -72,23 +32,19 @@ function Dashboard({ hasuraProps, systemProps, name, mode, userConfig, setUserCo
       },
       {
         name: "Text Element",
-        position: {
-          x: 6,
-          y: 0
-        },
-        height: 9,
-        width: 6,
+        x: 6,
+        y: 0,
+        h: 9,
+        w: 6,
         text: "Dit is een klein stukje tekst. Prijs de heer, zuip wat meer!",
         type: elementType.STATIC
       },
       {
         name: "Video Element",
-        position: {
-          x: 0,
-          y: 9
-        },
-        height: 9,
-        width: 6,
+        x: 0,
+        y: 9,
+        h: 9,
+        w: 6,
         text: "https://archive.org/download/Rick_Astley_Never_Gonna_Give_You_Up/Rick_Astley_Never_Gonna_Give_You_Up.mp4",
         type: elementType.STATIC
       }]
@@ -97,10 +53,54 @@ function Dashboard({ hasuraProps, systemProps, name, mode, userConfig, setUserCo
 
   function getDashboard(name: string) {
     const dashboard = dashboards.filter((dashboard: any) => dashboard.name === name)[0];
+    console.log(name);
     if (dashboard) {
       return dashboard
     }
     return dashboards[0];
+  }
+ 
+  const dashboard = getDashboard(name);
+
+  function renderDashboardElement(element: any, hasuraProps: any, index: number): JSX.Element {
+
+    let rendered_element = <p>Unknown element type</p>;
+    switch (element.type) {
+      case elementType.GRIDVIEW:
+        rendered_element = <p>Gridview W.I.P.</p>;
+        break;
+      case elementType.STATIC:
+        rendered_element = <p>Static W.I.P.</p>;
+        break;
+    }
+    return (
+      <div
+        id="test_id"
+        key={index + Date.now()}
+        style={{ outline: "2px solid #ebf2ff" }}
+        data-grid={{
+          x: element.x,
+          y: element.y,
+          w: element.w,
+          h: element.h,
+          static: mode === workspaceStates.DISPLAY_DASHBOARD
+        }}
+      >
+        {rendered_element}
+      </div>
+    );
+  }
+
+  const saveChange = (layout: any) => {
+    console.log("Change made:");
+    console.log(layout)
+    let newDashboard = dashboard;
+    for (let i = 0; i < newDashboard.dashboardElements.length; i++) {
+      newDashboard.dashboardElements[i].x = layout[i].x;
+      newDashboard.dashboardElements[i].y = layout[i].y;
+      newDashboard.dashboardElements[i].w = layout[i].w;
+      newDashboard.dashboardElements[i].h = layout[i].h;
+    }
   }
 
   const onDrop = (layout: any, layoutItem: any, _event: Event) => {
@@ -109,21 +109,23 @@ function Dashboard({ hasuraProps, systemProps, name, mode, userConfig, setUserCo
     console.log(_event)
   };
 
+
   return (
     <GridLayout
       className="layout"
       cols={12}
       rowHeight={30}
-      width={1500}
+      width={1500} // TODO: Make width scale responsively
       compactType={null}
       preventCollision={true}
       isDroppable={true}
       onDrop={onDrop}
+      onLayoutChange={(layout: any) => saveChange(layout)}
     >
       {
-        getDashboard(name)
+        dashboard
           .dashboardElements
-          .map((element: any, index) => renderDashboardElement(element, hasuraProps, index))
+          .map((element: any, index: number) => renderDashboardElement(element, hasuraProps, index))
       }
     </GridLayout>
   );
