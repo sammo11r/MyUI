b2f3302f7d0013d6d333a89b8be4a270371532f5
Hellenbrand, J.W.F <j.w.f.hellenbrand@student.tue.nl>
2022-06-04 12:13:10 +0300

    [#62] Language button

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "antd/dist/antd.css";
-import { Menu, Dropdown, Space } from "antd";
+import { Menu, Dropdown, Space, Radio } from "antd";
 import {
   UserOutlined,
   SettingFilled,
@@ -10,14 +10,63 @@ import { Content, Header } from "antd/lib/layout/layout";
 import { signOut } from "next-auth/react";
 import { useTranslation } from "next-i18next";
 
+import i18next from "i18next";
+import type { RadioChangeEvent } from 'antd';
+import { useRouter } from 'next/router'
+
 /**
- * @return {*}
+ * Update the language in i18next
+ * 
+ * @param {string} lan
+ * @return {*} 
  */
-function AppHeader() {
+function languageUpdate(lan: string) {
+  i18next.init();
+  i18next.changeLanguage(lan, (err) => {
+    if (err) return console.log('Something went wrong while loading the language', err);
+  });
+}
+
+/**
+ * @param {*} { 
+ *   userConfig,
+ *   setUserConfig,
+ *   userConfigQueryInput,
+ *   setUserConfigQueryInput
+ * }
+ * @return {*} 
+ */
+export default function AppHeader({
+  userConfig,
+  setUserConfig,
+  userConfigQueryInput,
+  setUserConfigQueryInput
+}: any): JSX.Element {
   const { t } = useTranslation();
+
+  const router = useRouter();
+  const { pathname, asPath, query } = router;
+
+  // Update the locale
+  const changeLocale = (e: RadioChangeEvent) => {
+    // Update the language in i18next
+    const localeValue = e.target.value;
+    languageUpdate(localeValue);
+
+    // Update the language in userConfig
+    userConfig.uiPreferences.language = e.target.value;
+    setUserConfig(userConfig);
+    setUserConfigQueryInput(userConfig);
+
+    // Push the changes
+    router.push({ pathname, query }, asPath, { locale: localeValue })
+  };
+
+  // Define the default user menu configuration
   const userMenu = (
     <Menu
       items={[
+        // Logout button
         {
           label: (
             <Space>
@@ -27,6 +76,27 @@ function AppHeader() {
           ),
           onClick: () => signOut({ callbackUrl: "/" }),
           key: 0,
+        },
+        // Change language buttons
+        {
+          label: (
+            <div>
+              <span>{t("logout.language")}</span>
+              <div>
+                <Radio.Group defaultValue={userConfig ? userConfig.uiPreferences.language : 'en'} onChange={changeLocale}>
+                  <Space direction="vertical">
+                    <Radio.Button value='en'>
+                      {t("logout.english")} <span role="img" aria-label="enFlag">🇺🇸</span>
+                    </Radio.Button>
+                    <Radio.Button value='nl'>
+                      {t("logout.dutch")}  <span role="img" aria-label="nlFLag">🇳🇱</span>
+                    </Radio.Button>
+                  </Space>
+                </Radio.Group>
+              </div>
+            </div>
+          ),
+          key: 1,
         },
       ]}
     />
@@ -51,7 +121,7 @@ function AppHeader() {
       <Dropdown
         overlay={userMenu}
         trigger={["click"]}
-        placement="bottom"
+        placement="bottomRight"
         arrow={{ pointAtCenter: true }}
       >
         <a
@@ -74,5 +144,3 @@ function AppHeader() {
     </Header>
   );
 }
-
-export default AppHeader;
