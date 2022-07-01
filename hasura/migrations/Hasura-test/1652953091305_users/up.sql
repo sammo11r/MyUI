ea45e5ac9b458ad28bf4783e21e7a0b3ed8efc60
Guha, N <n.guha1@student.tue.nl>
2022-07-01 13:40:11 +0200

    fix(migrations)

--- hasura/migrations/Hasura-test/1652953091305_users/up.sql
+++ hasura/migrations/Hasura-test/1652953091305_users/up.sql
@@ -18,13 +18,3 @@ FOR EACH ROW
 EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
 COMMENT ON TRIGGER "set_public_users_updated_at" ON "public"."users" 
 IS 'trigger to set value of column "updated_at" to current timestamp on row update';
-
-INSERT INTO "public"."users"("id", "name", "email", "created_at", "updated_at") VALUES (1, E'Demo user 1', E'demoemail1@domain.com', E'2022-05-19T09:37:46.250791+00:00', E'2022-05-19T09:37:46.250791+00:00');
-
-INSERT INTO "public"."users"("id", "name", "email", "created_at", "updated_at") VALUES (2, E'Demo user 2', E'demoemail2@domain.com', E'2022-05-19T09:37:55.471197+00:00', E'2022-05-19T09:37:55.471197+00:00');
-
-INSERT INTO "public"."users"("id", "name", "email", "created_at", "updated_at") VALUES (3, E'Demo user 3', E'demoemail3@domain.com', E'2022-05-19T09:38:01.858341+00:00', E'2022-05-19T09:38:01.858341+00:00');
-
-INSERT INTO "public"."users"("id", "name", "email", "created_at", "updated_at") VALUES (4, E'Demo user 4', E'demoemail4@domain.com', E'2022-05-19T09:38:06.79263+00:00', E'2022-05-19T09:38:06.79263+00:00');
-
-INSERT INTO "public"."users"("id", "name", "email", "created_at", "updated_at") VALUES (5, E'Demo user 5', E'demoemail5@domain.com', E'2022-05-19T09:38:11.294273+00:00', E'2022-05-19T09:38:11.294273+00:00');
--- hasura/migrations/Hasura-test/1653581048301_passwordUpdate/up.sql
+++ hasura/migrations/Hasura-test/1653581048301_passwordUpdate/up.sql
@@ -4,8 +4,8 @@ alter table "public"."users" add column "password" text
 
 alter table "public"."users" drop column "email" cascade;
 
-INSERT INTO "public"."users"("id", "name", "created_at", "updated_at", "password") VALUES (6, E'admin', E'2022-05-26T16:03:09.052673+00:00', E'2022-05-26T16:03:09.052673+00:00', E'$2b$10$owg26lz6BEqx2IpdMCfwo.2oPyMU2CFlgBs565sSOy82p5RF2GybG');
+INSERT INTO "public"."users"("id", "name", "created_at", "updated_at", "password") VALUES (1, E'admin', E'2022-05-26T16:03:09.052673+00:00', E'2022-05-26T16:03:09.052673+00:00', E'$2b$10$owg26lz6BEqx2IpdMCfwo.2oPyMU2CFlgBs565sSOy82p5RF2GybG', E'admin');
 
-INSERT INTO "public"."users"("id", "name", "created_at", "updated_at", "password") VALUES (7, E'editor', E'2022-05-26T16:03:25.037335+00:00', E'2022-05-26T16:03:25.037335+00:00', E'$2b$10$.q5d8gdb4oR/kDi.dzLHv.Bd/ReLgipOJLxayyt7/lwa6u59sNDee');
+INSERT INTO "public"."users"("id", "name", "created_at", "updated_at", "password") VALUES (2, E'editor', E'2022-05-26T16:03:25.037335+00:00', E'2022-05-26T16:03:25.037335+00:00', E'$2b$10$.q5d8gdb4oR/kDi.dzLHv.Bd/ReLgipOJLxayyt7/lwa6u59sNDee', E'editor');
 
-INSERT INTO "public"."users"("id", "name", "created_at", "updated_at", "password") VALUES (8, E'user', E'2022-05-26T16:04:08.282555+00:00', E'2022-05-26T16:04:08.282555+00:00', E'$2b$10$ABzgtZoFp2WqDhKGPD41MesAxgCToFVn0lmdpK2KXgDOznQsXnlLC');
+INSERT INTO "public"."users"("id", "name", "created_at", "updated_at", "password") VALUES (3, E'user', E'2022-05-26T16:04:08.282555+00:00', E'2022-05-26T16:04:08.282555+00:00', E'$2b$10$ABzgtZoFp2WqDhKGPD41MesAxgCToFVn0lmdpK2KXgDOznQsXnlLC', E'user');
